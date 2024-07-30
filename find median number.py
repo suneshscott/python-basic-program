2a9b7a1b60076cbe5492580768f7bcbd75bb23b6
suneshscott <suneshscott@gmail.com>
2024-07-30 04:07:36 -0700

    Add files via upload

---
+++ find median number.py
@@ -0,0 +1,24 @@
+a=float(input())
+b=float(input())
+c=float(input())
+if a>b:
+    if a<c:
+        median=a
+
+    elif b>c:
+        median=b
+
+    else:
+        median=c
+
+else:
+    if a>c:
+        median=a
+
+    elif b<c:
+        median=b
+
+    else:
+        median=c
+
+print("the median is:",median)
